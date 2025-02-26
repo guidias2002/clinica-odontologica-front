83ecf7aa830f52b1eab0ada8652611e7d59817f5
guidias2002 <guidias200220@gmail.com>
2025-02-26 12:17:49 -0300

    feat: add theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-
+import { ThemeProvider } from '@mui/material'
 import './App.css'
 import ConsultationForm from './components/consultation/ConsultationForm'
+import theme from './theme'
 
 function App() {
 
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <ConsultationForm/>
-    </>
+    </ThemeProvider>
   )
 }
 
