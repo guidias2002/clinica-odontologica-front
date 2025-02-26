02bb9ccc9910fda7cd03e9321fc2a0ba4864458e
guidias2002 <guidias200220@gmail.com>
2025-02-26 20:41:32 -0300

    feat: update css

--- src/theme.ts
+++ src/theme.ts
@@ -7,62 +7,20 @@ const theme = createTheme({
         },
     },
     components: {
-        MuiTextField: {
-            styleOverrides: {
-                root: {
-                    backgroundColor: 'rgba(131, 56, 236, 0.05)',
-                    border: 'none',
-                    borderRadius: '8px',
-                    '& .MuiOutlinedInput-root': {
-                        '&:hover fieldset': {
-                            borderColor: '#8338EC',
-                        },
-                        '&.Mui-focused fieldset': {
-                            borderColor: '#8338EC',
-                        },
-                    },
-                },
-            },
-        },
-        MuiSelect: {
-            styleOverrides: {
-                root: {
-                    backgroundColor: 'rgba(131, 56, 236, 0.05)',
-                    border: 'none',
-                    borderRadius: '8px',
-                    '& .MuiOutlinedInput-root': {
-                        '&:hover fieldset': {
-                            borderColor: '#8338EC', // Borda roxa ao passar o mouse
-                        },
-                        '&.Mui-focused fieldset': {
-                            borderColor: '#8338EC', // Borda roxa quando em foco
-                        },
-                    },
-                },
-            },
-        },
         MuiOutlinedInput: {
             styleOverrides: {
                 root: {
-                    backgroundColor: 'rgba(131, 56, 236, 0.05)', // BG com 5% de opacidade
-                    borderRadius: '8px',
-                    '& fieldset': {
-                        borderColor: 'rgba(131, 56, 236, 0.5)', // Cor inicial da borda
-                    },
-                    '&:hover fieldset': {
-                        borderColor: '#8338EC', // Borda roxa ao passar o mouse
-                    },
-                    '&.Mui-focused fieldset': {
-                        borderColor: '#8338EC', // Borda roxa quando focado
-                    },
-                },
-            },
-        },
-        MuiMenuItem: {
-            styleOverrides: {
-                root: {
                     backgroundColor: 'rgba(131, 56, 236, 0.05)',
                     borderRadius: '8px',
+                    '& .MuiOutlinedInput-notchedOutline': {
+                        borderColor: 'rgba(131, 56, 236, 0.5)', // Cor inicial da borda
+                    },
+                    '&:hover .MuiOutlinedInput-notchedOutline': {
+                        borderColor: '#8338EC !important', // Forçando a cor no hover
+                    },
+                    '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
+                        borderColor: '#8338EC !important', // Forçando a cor ao focar
+                    },
                 },
             },
         },
